6b92a52c74a80c2f96ffc1691a17ebfe1c3bf89e
Sherlock Tang <280320889@qq.com>
2025-04-23 13:52:07 +0800

    ✨ Feat: #D Astronaut
    add 3d Astronaut in herosection

--- src/lib/components/index/body/Hero.svelte
+++ src/lib/components/index/body/Hero.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import HeroText from "$lib/components/body/hero/HeroText.svelte"
   import ParallaxBackground from "../../body/hero/ParallaxBackground.svelte"
+  import Astronaut from "$lib/components/body/hero/Astronaut.svelte"
+  import { T } from "@threlte/core"
+  import { Canvas } from "@threlte/core"
+  // 移除 OrbitControls 导入，因为我们不再需要它
 </script>
 
 <section
@@ -8,6 +12,13 @@
 >
   <HeroText />
   <ParallaxBackground />
+  <figure class="absolute inset-0" style="width:100vw;height:100vh;pointer-events:auto;z-index:10;">
+    <Canvas>
+      <T.PerspectiveCamera position={[0, 0.5, 15]} makeDefault />
+      <Astronaut />
+    </Canvas>
+
+  </figure>
 </section>
 
 <style lang="postcss">
